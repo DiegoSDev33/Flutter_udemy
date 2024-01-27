82bda98641e29d08078f6f05f244e0bd96cc13ea
Diego Santos <diegodev.silva@gmail.com>
2024-01-26 21:50:39 -0300

    criando botoes e lista de perguntas

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 main() => runApp(PerguntaApp());
 
+final List<String> perguntas = [
+  'Qual é a sua cor favorita?',
+  'Qual é o seu animal favorito?',
+];
 
 class PerguntaApp extends StatelessWidget {
   @override
@@ -11,7 +15,23 @@ class PerguntaApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Ola meu body'),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Resposta 3'),
+            ),
+          ],
+        ),
       ),
     );
   }
