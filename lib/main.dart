7e2d39364e1491b34eb3a56c8cf335aa1ad105ea
Diego Santos <diegodev.silva@gmail.com>
2024-01-26 21:17:46 -0300

    Hello World

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(new PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  Widget build(BuildContext) {
+    return MaterialApp(
+      home: Text('Ola mundo'),
+    );
+  }
+}
