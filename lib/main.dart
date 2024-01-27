5ba1bea6f31b3aefcd50a3e6b588afce613de5d2
Diego Santos <diegodev.silva@gmail.com>
2024-01-26 21:30:33 -0300

    arvore de widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(new PerguntaApp());
-}
+main() => runApp(PerguntaApp());
+
 
 class PerguntaApp extends StatelessWidget {
-  Widget build(BuildContext) {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Ola mundo'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Text('Ola meu body'),
+      ),
     );
   }
 }
