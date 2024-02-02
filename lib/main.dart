9d270c891b8c18083d59a545a80da60f0a51d632
Diego Santos <diegodev.silva@gmail.com>
2024-02-01 21:11:20 -0300

    atualizacao

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
-
+import './questao.dart';
 main() => runApp(PerguntaApp());
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+
+  void _responder() {
+    setState(() {
+      _perguntaSelecionada++;
+    });
+    print(_perguntaSelecionada);
+  }
+
 
 final List<String> perguntas = [
   'Qual é a sua cor favorita?',
   'Qual é o seu animal favorito?',
 ];
 
-class PerguntaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,22 +27,28 @@ class PerguntaApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Text(perguntas[0]),
+            Questao(perguntas[_perguntaSelecionada]),  
             ElevatedButton(
-              onPressed: () {},
+              onPressed: _responder,
               child: Text('Resposta 1'),
             ),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: _responder,
               child: Text('Resposta 2'),
             ),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: _responder,
               child: Text('Resposta 3'),
             ),
           ],
         ),
       ),
     );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
   }
 }
